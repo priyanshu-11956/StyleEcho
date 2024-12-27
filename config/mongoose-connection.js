257d727c70d25b87df2934615044695d41bd3861
Priyanshu-11956 <priyanshu11956@gmail.com>
2024-12-27 18:48:21 +0530

    added more depth routing

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose")
-
+const dbgr = require("debug")("development:mongoose");
+const config = require("config")
 mongoose
-.connect("mongodb://localhost:27017/")
-.then(() => console.log("Connected to MongoDB"))
-.catch((err) => console.log(err))
-const db = mongoose.connection
-db.on("error", (err) => console.log(err))
+.connect(`${config.get("MONGODB_URI")}/`)
+.then(() => dbgr("Connected to MongoDB"))
+.catch((err) => dbgr(err))
 module.exports = mongoose.connection;
--- routes/ownersRouter.js
+++ routes/ownersRouter.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
-router.get("/", function(req, res){
-    res.send("hey!");
+const ownerModel = require("../models/owner-model");
+
+if (process.env.NODE_ENV === "development") {
+  router.post("/create", async function (req, res) {
+    let owners = await ownerModel.find();
+    if (owners.length > 0) {
+      return res
+        .send("503")
+        .send("you don't have permission to createa new owner.");
+    }
+    res.send("you can create a new owner.");
+  });
+}
+router.get("/", function (req, res) {
+  res.send("hey!");
 });
-module.exports = router; 
+router.post("/create", function (req, res) {
+  res.send("hey!");
+});
+module.exports = router;
